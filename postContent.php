d2c93fbe9e2062964418833eb29d3660d7dfb17b
Miro Haapalainen <32234658+Merplop@users.noreply.github.com>
2022-03-24 23:46:43 -0700

    Update postContent.php

--- postContent.php
+++ postContent.php
@@ -2,28 +2,14 @@
 <body>
 
 <?php 
-
-	$servername = "localhost";
-	$username = 'netuser';
-	$password = 'darthgamer';
-	$dbname = 'netbase';
-
 	$title = $_POST["title"];
 	$content = $_POST["content"];
 
-	echo $title;
-	echo $content;
-
-	$conn = new mysqli($servername, $username, $password, $dbname);
-
-	if ($conn->connect_error) {
-		echo "Connection refused";
-		die("Connection refused" . $conn->connect_error);
-	}
+	include_once ("logIn.php");
 
 	$sql = "INSERT INTO Posts (id, userID, title, postContent, createdDate) VALUES (0, 0, '" . $title . "', '" . $content . "', '2022-03-03 00:00:00')";
-	echo $sql;
-	$result = $conn->query($sql);
+	$result = logIn($sql);
+
 ?>
 </body>
 </html>
