74fb1fbd3118d48a7d574d2b4e68ab78e0803321
Miro Haapalainen <32234658+Merplop@users.noreply.github.com>
2022-04-21 13:39:30 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,70 @@
-<body>
+<?php
+	include_once 'header.php'; 
+?>
 
-<h1>Netbase Home</h1>
-
-<form action="postContent.php" method="post">
-	<label for="title">Title</label><br>
-	<input type="text" id="title" name="title"><br>
-	<label for="content">Post content:</label><br>
-	<input type="text" id="content" name="content"><br>
-	<input type="submit">
+<form>
+<textarea name="cpost"></textarea>
+<input type=submit value="Post">
 </form>
 
 <?php
 
 include_once ("logIn.php");
 
-$sql = "SELECT id, userId, title, postContent, createdDate FROM Posts;";
-$result = logIn($sql);
+function getUID() {
+	$ip = $_SERVER['REMOTE_ADDR'];
+	$uid = getSingle("select uid from users where ip = '".$ip."'");
+		if(!$uid) {
+			logIn("insert into users(ip) values ('$ip')");
+	}
+	return $uid;
+}
 
+if($_REQUEST['cpost']) {
+	$postContent = $_REQUEST['cpost'];
+	$ip = $_SERVER['REMOTE_ADDR'];
+	$uid = getUID();
+	$date = Date("Y-m-d H:i:s");
+	logIn("insert into posts (uid, post, date) values($uid, '$postContent', '$date')");
+}
+
+function getSingle($query) {
+	$result = logIn($query);
+	$row = mysqli_fetch_row($result);
+	return $row[0];
+}
+
+if($_REQUEST['follow']) {
+	$follow = $_REQUEST['follow'];
+	$uid = getUID();
+	logIn("insert ignore into follows(uid, follower) values ($uid, $follow)");
+}
+
+?> 
+<h2>Recent Posts</h2>
+<?php
+
+$result = logIn("select * from posts order by date desc");
+print "<table border=1>";
+while ($row = mysqli_fetch_assoc($result)) {
+	$uid = $row['uid'];
+	$postContent = htmlspecialchars($row['post']);
+	$date = $row['date'];
+
+	$follow = <<<EOF
+	<a href=index.php?follow=$uid>Follow</a>
+	EOF;
+	print <<<EOF
+
+	<tr><td>$uid</td><td>$postContent</td><td>$date</td><td>$follow</td></tr>
+	EOF;
+
+}
+print "</table>";
 
 if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-                echo "Title: " . $row["title"]. " - Name: " . $row["userId"]. " " . $row["postContent"]. " " . $row["createdDate"]. "<br>";
+                print_r($row);
         }
 } else {
         echo "0 results";
