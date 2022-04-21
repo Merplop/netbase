f200253f82fe13c877dc9ee3ebc1441e8e3f2619
Miro Haapalainen <32234658+Merplop@users.noreply.github.com>
2022-04-21 13:40:55 -0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	
+<link rel="stylesheet" href="style.css">
+
+<ul>
+	<li><a href="index.php">Home</a></li>
+	<li><a href="userLogIn.php">Log In</a></li>
+	<li><a href="userRegister.php">Register</a></li>
+</ul>
+
+<h1>Netbase</h1>
+</head>
+
+<body>
+
+<div class="wrapper">
