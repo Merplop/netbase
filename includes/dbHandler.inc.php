edadf5a3d990dfa5cab119f70bed19d12b52b8bc
Miro Haapalainen <32234658+Merplop@users.noreply.github.com>
2022-05-06 14:19:08 -0700

    Create dbHandler.inc.php

---
+++ includes/dbHandler.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+	function logIn($sql) {
+		$servername = "localhost";
+		$username = 'netuser';
+		$password = 'darthgamer';
+		$dbname = 'netbase';
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		if ($conn->connect_error) {
+			echo "Connection refused";
+			die("Connection refused" . $conn->connect_error);
+		}
+		$result = $conn->query($sql);
+		return $result;
+	}
+?>
