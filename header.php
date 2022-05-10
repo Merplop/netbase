941ce2179ac05fde87e0c67bbdfbca190e355b8c
Miro Haapalainen <32234658+Merplop@users.noreply.github.com>
2022-05-10 11:11:00 -0700

    Update header.php

--- header.php
+++ header.php
@@ -1,15 +1,32 @@
+<?php 
+	session_start();
+?>
+
 <html>
 <head>
 	
 <link rel="stylesheet" href="style.css">
 
 <ul>
-	<li><a href="index.php">Home</a></li>
-	<li><a href="userLogIn.php">Log In</a></li>
-	<li><a href="userRegister.php">Register</a></li>
+	<li><a class="active" href="index.php">Home</a></li>
+	<?php 
+		if (isset($_SESSION["uid"])) {
+			echo "<li><a href='myProfile.php'>My Profile</a></li>";
+			echo "<li><a href='logout.php'>Log Out</a></li>";
+		} else {
+			echo "<li><a href='userLogIn.php'>Log In</a></li>";
+			echo "<li><a href='userRegister.php'>Register</a></li>";
+		}
+	?>
 </ul>
 
 <h1>Netbase</h1>
+
+<?php 
+	if (isset($_SESSION["uid"])) {
+		echo "You are signed in as: " . $_SESSION["username"];
+	}
+?>
 </head>
 
 <body>
